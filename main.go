a5a930e0d604bb936579171801979fa1d96e1a9f
JrGustavo <gus@soypetal.com>
2024-12-18 21:34:41 -0500

    Update practice Go

--- main.go
+++ main.go
@@ -3,11 +3,18 @@ package main
 import (
 	"fmt"
 	"github.com/JrGustavo/practice_go/variables"
+	"runtime"
 )
 
 func main() {
 	estado, texto := variables.ConviertoATexto(1588)
-	fmt.Print(estado)
-	fmt.Print(texto)
+	fmt.Println(estado)
+	fmt.Println(texto)
+	os := runtime.GOOS
+	if os == "Linux." {
+
+	} else {
+		fmt.Println("No es Linux")
+	}
 
 }
